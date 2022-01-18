2decde68dd158df48e3b3a49e5cca330d964414e
heon <siwmua0@naver.com>
2022-01-19 05:15:13 +0900

    updateviews

---
+++ accountapp/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+
+
+# 상속을받아서 >> 커스텀아이징 클래스
+
+class AccountUpdateForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        
+        self.fields['username'].disabled = True
+
+        
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,9 @@
 from django.urls import reverse, reverse_lazy
 from django.http import*
 from django.shortcuts import render
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import MyTable
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -42,5 +43,11 @@ class AccountCreateView(CreateView):
     
 class AccountDetailView(DetailView):
     model = User
-    context_object_name = 'target_user'
+    context_object_name = 'target_user' #쉽게말해서 이 클래스를 생성한 유저 (주인의 이름)
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User ## 장고에서 기본으로 지원하는 모델
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:test') # reverse든 lazy이든 (app, pathname)
+    template_name = 'accountapp/update.html'
